cb47c18444864ea06835d52610d64b1d981ab75d
Herimanjaka Razakasoa <geit.herimanjaka@gmail.com>
2024-11-10 13:10:42 +0300

    ajout du main pour le test

--- main.dart
+++ main.dart
@@ -1,1 +1,17 @@
-void main(List<String> args) {}
+import 'Researcher.dart';
+import 'Teacher.dart';
+import 'TemporaryTeacher.dart';
+import 'pHD.dart';
+
+void main(List<String> args) {
+  var geitTeacher = <Teacher>[
+    Researcher("Tsinto", "Rakoto", 160, 400),
+    Researcher("Santatra", "Razaka", 40, 300),
+    Temporaryteacher("Tolotra", "Randria", 30, 700, "Hairun Tech"),
+    Phd("Roger", "Nary", 80, 350, 60)
+  ];
+  var salaryPerTeacher = geitTeacher.map((teacher) => teacher.currentCost());
+  var totalSalary =
+      salaryPerTeacher.fold(0.0, (prev, element) => (prev + element) * 1.0);
+  print("Total cost : $totalSalary euro");
+}
